902b04022ad88643acbe9bf13847ba7ecdc2dea0
Vladmir-JJ <jjwysocki89@gmail.com>
2022-01-12 11:52:29 +0100

    visual scripts

---
+++ Assets/_Scripts/TheLooks/SkyRoller.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace JJ.STG.Looks
+{
+    public class SkyRoller : MonoBehaviour
+    {  
+        void Update()
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(0, -19f, 5), Time.deltaTime);
+        }
+    }
+}
